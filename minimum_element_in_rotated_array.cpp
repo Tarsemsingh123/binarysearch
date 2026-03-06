242c4b4c5b30155a705e71eaa4ef9d482f021c6c
Tarsem singh <tarsemchopra20@gmail.com>
2026-03-06 10:23:27 +0530

    my forth commit

--- minimum_element_in_rotated_array.cpp
+++ minimum_element_in_rotated_array.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
 using namespace std; 
 
-int PeakElement(int arr[],int n){
+
+int Minimum_element_at_roted_array(int arr[],int n){
     int start=0,mid;
     int end=n-1;
+    int ans=arr[0];
     //first condition
     while(start<=end){
 
    int  mid=start+(end-start)/2;
 
 
-if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
-    return mid;
-    break;
-}
-else if(arr[mid]>arr[mid-1]){
+if(arr[mid]>=arr[0]){
     start=mid+1;
-
 }
-else{
-    end=end-1;
+else {
+    ans =arr[mid];
+    end=mid-1;
+
 }
 
     }
-    return -1;
+    return ans;
 }
-
 
 
 int main(){
@@ -41,7 +39,6 @@ for(int i=0;i<n;i++){
     cin>>arr[i];
 }
 
-cout<<PeakElement(arr,n);
-    
+cout<<Minimum_element_at_roted_array(arr,n);
 return 0;
 }
